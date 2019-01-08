0121cf64c8886a966358993b2f6edef1cdf0a1fe
Bennett, Thomas <thomas.bennett@sky.uk>
2019-01-08 14:22:30 +0000

    helm chart

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo 'Deploying...'
+
+helm upgrade -i article-feed ./article-feed \n
+--set image.tag=${CIRCLE_BUILD_NUM} \n
+--set port=${PORT} --set server=${SERVER} \n
+--set db=${DB} \n
+--set articleCollection=${ARTICLE_COLLECTION} \n
+--set dbUser=${DB_USER} \n
+--set dbPassword=${DB_PASSWORD}
