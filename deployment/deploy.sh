c56cc0e8440097cfc89e37ff4ef18311ac81888b
Bennett, Thomas <thomas.bennett@sky.uk>
2019-01-09 08:47:44 +0000

    fix formatting in deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,12 +2,4 @@
 
 echo 'Deploying...'
 
-helm upgrade -i article-feed ./article-feed \n
---set image.tag=${CIRCLE_BUILD_NUM} \n
---set port=${PORT} --set server=${SERVER} \n
---set db=${DB} \n
---set articleCollection=${ARTICLE_COLLECTION} \n
---set dbUser=${DB_USER} \n
---set dbPassword=${DB_PASSWORD} \n
---set authDb=${AUTH_DB} \n
---set dbSsl=${DB_SSL}
+helm upgrade -i article-feed ./article-feed --set image.tag=${CIRCLE_BUILD_NUM} --set port=${PORT} --set server=${SERVER} --set db=${DB} --set articleCollection=${ARTICLE_COLLECTION} --set dbUser=${DB_USER} --set dbPassword=${DB_PASSWORD} --set authDb=${AUTH_DB} --set dbSsl=${DB_SSL}
