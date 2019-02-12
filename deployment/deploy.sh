24b5260e6fdc43364c57ab73d38c5e78f115339b
Bennett, Thomas <thomas.bennett@sky.uk>
2019-02-12 08:17:54 +0000

    Correct path to helm chart in deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -21,4 +21,4 @@ gcloud --quiet container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
 
 ## deploy helm chart
 
-helm upgrade -i article-feed ./article-feed --set image.tag=${CIRCLE_BUILD_NUM} --set port=${PORT} --set server=${SERVER} --set db=${DB} --set articleCollection=${ARTICLE_COLLECTION} --set dbUser=${DB_USER} --set dbPassword=${DB_PASSWORD} --set authDb=${AUTH_DB} --set dbSsl=${DB_SSL}
+helm upgrade -i article-feed ./article-feed-k8s --set image.tag=${CIRCLE_BUILD_NUM} --set port=${PORT} --set server=${SERVER} --set db=${DB} --set articleCollection=${ARTICLE_COLLECTION} --set dbUser=${DB_USER} --set dbPassword=${DB_PASSWORD} --set authDb=${AUTH_DB} --set dbSsl=${DB_SSL}
