fb4f5f911fde2282cc2f18cee3f38d35b690228b
Bennett, Thomas <thomas.bennett@sky.uk>
2019-05-14 15:39:13 +0100

    helm deplot

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo 'Deploying...'
+## gcloud login
+gcloud auth activate-service-account --key-file=auth.json
+
+## get kubeconfig
+echo "Saving config to cache..."
+mkdir -p site-config
+echo $GCLOUD_SERVICE_KEY > ./site-config/auth.json
+gsutil cp gs://article-app-storage/kubeconfig ./site-config/kubeconfig
 
 ## install helm
 echo "Check Helm is installed..."
@@ -9,17 +16,40 @@ if [[ $((helm) 2>&1 | grep "command not found" ) ]]; then
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
     chmod 700 get_helm.sh
     ./get_helm.sh
+    helm init --upgrade --wait --kubeconfig ./site-config/kubeconfig
+    helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 fi
 
-## authenticate with GKE
-echo "Authenticating with GKE..."
-apt-get install -qq -y gettext
-echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
-gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
-gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
+## create namespace
+echo "Creating app namespace..."
+kubectl apply -f ./article-feed-k8s/namespace.yml --kubeconfig ./site-config/kubeconfig
 
 ## deploy helm chart
 echo "Deploying helm chart..."
-helm upgrade -i article-feed ./article-feed-k8s --set image.repository=${DOCKER_IMAGE_URL} --set image.tag=${CIRCLE_SHA1} --set port=${PORT} --set server=${SERVER} --set db=${DB} --set articleCollection=${ARTICLE_COLLECTION} --set dbUser=${DB_USER} --set dbPassword=${DB_PASSWORD} --set authDb=${AUTH_DB} --set dbSsl=${DB_SSL} --namespace article-app
+helm upgrade -i article-feed ./article-feed-k8s \
+    --set image.repository=${DOCKER_IMAGE_URL} \
+    --set image.tag=${CIRCLE_SHA1} --set port=${PORT} \
+    --set server=${SERVER} \
+    --set db=${DB} \
+    --set articleCollection=${ARTICLE_COLLECTION} \
+    --set dbUser=${DB_USER} \
+    --set dbPassword=${DB_PASSWORD} \
+    --set authDb=${AUTH_DB} \
+    --set dbSsl=${DB_SSL} \
+    --namespace article-app \
+    --kubeconfig ./site-config/kubeconfig
+
+# terraform plan \
+#             -var "config_file=auth.json" \
+#             -var "image_repository=${DOCKER_IMAGE_URL}" \
+#             -var "image_tag=${CIRCLE_SHA1}" \
+#             -var "sources=${SOURCES}" \
+#             -var "server=${SERVER}" \
+#             -var "db=${DB}" \
+#             -var "port=${PORT}" \
+#             -var "article_collection=${ARTICLE_COLLECTION}" \
+#             -var "db_user=${DB_USER}" \
+#             -var "db_password=${DB_PASSWORD}" \
+#             -var "auth_db=${AUTH_DB}" \
+#             -var "db_ssl=${DB_SSL}" \
+#             -var "namespace=${NAMESPACE}" \
